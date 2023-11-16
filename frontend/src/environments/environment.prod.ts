4e042839b36db74db4f54fb025b474b51252f62d
Cristian Almendras <cristian@openix.com.ar>
2023-11-16 00:21:18 -0300

    limpieza del repo

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_base: 'http://localhost:3000/'
+  url_base: 'http://localhost:3001/',
+  firebaseConfig : {
+    apiKey: "AIzaSyDCdKYQC6o5vBzuAvJkFgSaUYnn6H-fN-4",
+    authDomain: "centrodesaludhuico-d74ce.firebaseapp.com",
+    projectId: "centrodesaludhuico-d74ce",
+    storageBucket: "centrodesaludhuico-d74ce.appspot.com",
+    messagingSenderId: "1072746273571",
+    appId: "1:1072746273571:web:d5c1787de9f9171b410a96",
+    measurementId: "G-QD0V0TXSSH"
+  }
 };
